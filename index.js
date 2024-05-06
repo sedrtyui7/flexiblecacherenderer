708dc2f94192d358a61ea676d45c1c0b63666cc4
sedrtyui7 <gs1jill2106@hotmail.com>
2024-05-07 04:36:56 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-function combine(n, k) {
-  const result = [];
-  backtrack(1, []);
-  return result;
-  function backtrack(start, current) {
-    if (current.length === k) {
-      result.push([...current]);
-      return;
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const numChar = num.toString();
+            if (isValid(board, i, j, numChar)) {
+              board[i][j] = numChar;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
+      }
     }
-    for (let i = start; i <= n; i++) {
-      current.push(i);
-      backtrack(i + 1, current);
-      current.pop();
+    return true;
+  }
+  function isValid(board, row, col, num) {
+    const boxRow = Math.floor(row / 3) * 3;
+    const boxCol = Math.floor(col / 3) * 3;
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === num ||
+        board[i][col] === num ||
+        board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num
+      )
+        return false;
     }
+    return true;
   }
 }
